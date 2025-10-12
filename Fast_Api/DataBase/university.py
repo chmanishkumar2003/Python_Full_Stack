cae8a43a49623cbca07c36a5cd2eeafba887a9fc
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-10-12 21:13:44 +0530

    university_db

--- Fast_Api/DataBase/university.py
+++ Fast_Api/DataBase/university.py
@@ -18,7 +18,7 @@ class StatusEnum(str, enum.Enum):
     inactive = "inactive"
     alumni = "alumni"
 
-#  SQLAlchemy model — matches EXACT MySQL 
+#  SQLAlchemy model — matches EXACT 
 class Student(Base):
     __tablename__ = "student"
     stu_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
