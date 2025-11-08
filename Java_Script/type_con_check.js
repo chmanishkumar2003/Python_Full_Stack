9a6b87b3387302a99559d358a31d3000fd0caf9f
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-08 17:53:40 +0530

    js

---
+++ Java_Script/type_con_check.js
@@ -0,0 +1,10 @@
+let x = "Pizza";
+let y = "Pizza";
+let z = "Pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
