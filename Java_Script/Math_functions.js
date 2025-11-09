9576bfaf65fd6d2397cc4420852841c71f6e6aec
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-09 18:43:20 +0530

    js

---
+++ Java_Script/Math_functions.js
@@ -0,0 +1,21 @@
+//Math functions
+let x = -3.421;
+let y = 2;
+let z = 1;
+
+z = Math.ceil(x);
+z = Math.floor(x);
+z = Math.round(x);
+z = Math.trunc(x);
+z = Math.pow(x, y);
+z = Math.sqrt(y);
+z = Math.log(x);
+z = Math.sin(x);
+z = Math.cos(x);
+z = Math.tan(x);
+z = Math.abs(x);
+z = Math.sign(x);
+let max = Math.max(x, y, z)
+
+let min = Math.min(x, y, z)
+console.log(min);
