7f23f4dfcc02ee79126bdeb7fa8fcc8cea72fa27
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-12 18:37:53 +0530

    js

--- Java_Script/ternary.js
+++ Java_Script/ternary.js
@@ -1,3 +1,11 @@
 let age=1;
 let message=age>=18? "You are eligible":"You are not eligible";
 console.log(message);
+
+let student=true;
+let m=student?"You are Student": "You are not a student";
+console.log(m);
+
+let amount=110;
+let discount=amount>=100? 10:0;
+console.log(`Your total bill $${amount-amount*(discount/100)}`);
