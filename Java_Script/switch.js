86d96b4a87e82e30a3436dd768a45fdb5715dd0b
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-13 19:51:34 +0530

    js

---
+++ Java_Script/switch.js
@@ -0,0 +1,30 @@
+let day=1;
+switch(day){
+    case 1:
+        console.log("It is monday");
+        break;
+    case 2:
+        console.log("It is Tuesday");
+        break;
+
+    case 3:
+        console.log("It is Wednesday");
+        break;
+    case 4:
+        console.log("It is Thursday");
+        break;
+    case 5:
+        console.log("It is Friday");
+        break;
+    case 6:
+        console.log("It is Saturday");
+        break;
+    case 7:
+        console.log("It is Sunday");
+        break;
+    case "pizza":
+        console.log("It is pizza day");
+        break;
+    default:
+        console.log(`${day} Is not a day`)
+    }
