e143d809394bec5f5f2583a2ec6af66f11c40b5d
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-13 19:52:20 +0530

    js

---
+++ Java_Script/string_methods.js
@@ -0,0 +1,20 @@
+//String Methods
+let user="Manish Kumar ";
+console.log(user.lastIndexOf("a"));
+
+console.log(user.indexOf('K'));
+console.log(user.length);
+us=user.trim();
+console.log(us.toLowerCase());
+console.log(us.toUpperCase());
+console.log(us.repeat(3));
+ps=user.startsWith(" ");
+console.log(ps)
+ps=user.endsWith(" ");
+console.log(ps)
+ps=user.includes(" ");
+console.log(ps)
+
+
+let ph="123-456-789"
+console.log(ph.replaceAll("-",""))
