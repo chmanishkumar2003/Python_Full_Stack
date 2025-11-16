e851e4e1a1776a7a82d6b7d99ad23d55ed5b5c85
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-16 18:04:36 +0530

    js

---
+++ Java_Script/functions/basic.js
@@ -0,0 +1,6 @@
+function birthday(user,age){
+  console.log("Happy birthday");
+  console.log(`happy birthday dear ${user}`);
+  console.log(`You are ${age} years old now`);
+}
+birthday("Harish",22);
