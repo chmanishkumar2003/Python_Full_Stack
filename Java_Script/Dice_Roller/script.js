0e5a17f273660855f63aaa61b6f1dfd5cc94c657
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-19 13:00:56 +0530

    js_8

---
+++ Java_Script/Dice_Roller/script.js
@@ -0,0 +1,17 @@
+function roll() {
+    const num = document.getElementById("i1").value;
+    const res = document.getElementById("res");
+    const img = document.getElementById("img");
+
+    const val = [];
+    const image = [];
+
+    for (let i = 0; i < num; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        val.push(value);
+        image.push(`<img src="dice/${value}.jpg" width="100">`);
+    }
+
+    res.textContent = `dice: ${val.join(", ")}`;
+    img.innerHTML = image.join('');
+}
