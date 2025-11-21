bd57c4e6e28ec50cc647a1527be19fc6440ae697
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-21 17:53:50 +0530

    Functions
    Added a callback example demonstrating asynchronous operations.

---
+++ Java_Script/functions/CallBack/Basic.js
@@ -0,0 +1,15 @@
+//Callback is a function that is passed as argument in another function
+/*It is used for asychronous operations
+        1)Reading files
+        2)Iterating with data bases
+        3)Network Requests*/
+
+sum(display,1,2);
+
+function sum(call,x,y){
+    let res=x+y;
+    call(res);
+}
+function display(res){
+    document.getElementById("h1").textContent=res;
+}
