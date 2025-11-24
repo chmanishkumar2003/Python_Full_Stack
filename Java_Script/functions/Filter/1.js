c31e95a8af1ab5a6253ec24dd36492eee329abcf
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-24 19:15:41 +0530

    Fu
    Implement filter function to create an array of even numbers.

---
+++ Java_Script/functions/Filter/1.js
@@ -0,0 +1,13 @@
+//Filter function creates a new array by filtering out elements
+let num=[1,2,3,4,5,6];
+let evenNum=num.filter(odd);
+
+console.log(evenNum);
+
+function even(item){
+    return item % 2 ===0;
+}
+
+function odd(item){
+    return item % 2!==0;
+}
