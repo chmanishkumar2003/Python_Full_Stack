fd856e46581ba7f99055d70c295473fe99314344
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-26 22:41:45 +0530

    Functions
    Added comments to clarify the use of the Rest parameter and the function's behavior.

--- Java_Script/functions/sum.js
+++ Java_Script/functions/sum.js
@@ -1,3 +1,4 @@
+// ... is the Rest parameter which is used for collection of all arguments into an array.
 function sum(...num){
     let res=0;
     for(let n of num){
@@ -7,4 +8,6 @@ function sum(...num){
 }
 
 const total=sum(1,2,3,45);
+// Here "1,2,3,45" is the value passed to the function as num parameter.
 console.log(`Your total is ${total}`);
+
