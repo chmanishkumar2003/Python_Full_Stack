38309cc28e68b63d69d0c33e290b91e0208c78f7
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-11-27 18:20:25 +0530

    Functions
    Added a function declaration example with setTimeout.

---
+++ Java_Script/functions/Function Decelaration/1.js
@@ -0,0 +1,7 @@
+/*Function decelartion = defines  a reusable block of code that performs a specific Task
+
+Function expression = A way to define functions as values or variables */
+
+setTimeout( function(){
+    console.log("Hello");
+},3000)
