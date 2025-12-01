293abeccc90cc671189897b23ca246b4994691b1
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-12-01 19:29:46 +0530

    OOPS
    Demonstrates variable swapping using destructuring.

---
+++ Java_Script/OOPS/Destruction/Swapping.js
@@ -0,0 +1,10 @@
+//Destructing = extract values from arrays or obejects ,and the assign them 
+//to variables in a convinent way
+//[]= to perform array destruction
+//{} = to perform object destruction 
+//Swapping variables
+let a=1;
+let b=2;
+[a,b]=[b,a]
+console.log(a);
+console.log(b);
