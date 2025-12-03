96de992171b6849f3c0c75bdfea9515e5e2d2a79
Chintakayala Manish Kumar <mchintak@gitam.in>
2025-12-03 18:36:35 +0530

    OOPS

--- Java_Script/OOPS/Destruction/Swapping.js
+++ Java_Script/OOPS/Destruction/Swapping.js
@@ -1,7 +1,3 @@
-//Destructing = extract values from arrays or obejects ,and the assign them 
-//to variables in a convinent way
-//[]= to perform array destruction
-//{} = to perform object destruction 
 //Swapping variables
 let a=1;
 let b=2;
