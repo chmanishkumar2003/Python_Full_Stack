3527f9784f6088828e1c036e9d954808d9eab8ea
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2025-12-08 17:28:55 +0530

    OOPS

---
+++ Java_Script/OOPS/Array/obj_map.js
@@ -0,0 +1,23 @@
+//Array of objects
+const fruits=[{name:"apple",time:5,cal:57},
+              {name:"banana",time:7,cal:90},
+              {name:"pine",time:3,cal:84},
+              {name:"custard",time:4,cal:94}];
+console.log(fruits[3].name);
+console.log(fruits[1].time);
+console.log(fruits[2].cal);
+
+fruits.push({name:"mango",time:3,cal:74});
+fruits.pop();
+fruits.splice(1,2);
+console.log(fruits);
+
+//map()
+const names=fruits.map(fruit=>fruit.name);
+console.log(names);
+
+const age=fruits.map(fruit=>fruit.time);
+console.log(age);
+
+const caliroes=fruits.map(fruit=>fruit.cal);
+console.log(caliroes);
