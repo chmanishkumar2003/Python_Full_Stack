f5dac700674675dbc09ec7fada97b3d6fee457bd
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2025-12-09 23:54:20 +0530

    Functions

---
+++ Java_Script/functions/date.js
@@ -0,0 +1,20 @@
+//Date Objects = objects that contain values that represents dates and times . These date 
+//              objects can be changed and formatted.
+
+//Date(year,month,day,hour,minute,second,ms
+const date = new Date();
+console.log(date);
+
+const date = new Date();
+const year = date.getFullYear();
+const mon = date.getMonth();
+const day =date.getDate();
+const hr = date.getHours();
+const min = date.getMinutes();
+
+console.log(date);
+console.log(day);
+console.log(mon);
+console.log(year);
+console.log(hr);
+console.log(min);
