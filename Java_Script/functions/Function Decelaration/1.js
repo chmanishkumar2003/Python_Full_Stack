c42a4eac091cb45d80b2956104635daff75c2dae
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2025-12-15 22:12:38 +0530

    Functions

--- Java_Script/functions/Function Decelaration/1.js
+++ Java_Script/functions/Function Decelaration/1.js
@@ -1,6 +1,6 @@
-/*Function decelartion = defines  a reusable block of code that performs a specific Task
+// /*Function decelartion = defines  a reusable block of code that performs a specific Task
 
-Function expression = A way to define functions as values or variables */
+// Function expression = A way to define functions as values or variables */
 
 setTimeout( function(){
     console.log("Hello");
