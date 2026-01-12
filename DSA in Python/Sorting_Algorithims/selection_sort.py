cceefc0bb38c4198079e7d0f442537ec946435a4
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2026-01-12 15:45:21 +0530

    DSA

--- DSA in Python/Sorting_Algorithims/selection_sort.py
+++ DSA in Python/Sorting_Algorithims/selection_sort.py
@@ -1,15 +1,3 @@
-# Selection Sort
-# It selects the min element in an array and swaps it to first index.
-# arr=[4,2,3,6,1,5]
-# n=len(arr)
-# for i in range(n):
-#     min=i
-#     for j in range(i+1,n):
-#         if arr[min] > arr[j]:
-#             min=j                       #Update Minimum value
-#     arr[i],arr[min]=arr[min],arr[i]     #Swaps the number outside the loop.
-# print(arr)
-
 # Selection Sort using Function
 def sel(arr):
     for i in range(len(arr)):
