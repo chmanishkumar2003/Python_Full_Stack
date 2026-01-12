dd47bb5b13ea27d4a43700f51a2e0099c7fdc55d
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2026-01-12 12:17:22 +0530

    DSA

--- DSA in Python/Sorting_Algorithims/insertion_sort.py
+++ DSA in Python/Sorting_Algorithims/insertion_sort.py
@@ -1,32 +1,19 @@
-# Insertion Sort
-arr=[1,4,3,2,5,8]
-n=len(arr)
-for i in range(n):
-    key=arr[i]
-    pos=i
-    for j in range(i-1,-1,-1):
-        if arr[j]> key:
-            arr[j+1]=arr[j]         #shift
-            pos=j
-        else:
-            break
-    arr[pos]=key
-print(arr)
+
 
 # Insertion Sort using Function
-# def ins(arr):
-#     n=len(arr)
-#     for i in range(n):
-#         key=arr[i]
-#         pos=i
-#         for j in range(i-1,-1,-1):
-#             if arr[j]>key:
-#                 arr[j+1]=arr[j]
-#                 pos=j            
-#             else:
-#                 break
-#         arr[pos]=key
-#     return arr
+def ins(arr):
+    n=len(arr)
+    for i in range(n):
+        key=arr[i]
+        pos=i
+        for j in range(i-1,-1,-1):
+            if arr[j]>key:
+                arr[j+1]=arr[j]
+                pos=j            
+            else:
+                break
+        arr[pos]=key
+    return arr
 
-# arr=[1,4,2,5,3,6]
-# print(ins(arr))
+arr=[1,4,2,5,3,6]
+print(ins(arr))
