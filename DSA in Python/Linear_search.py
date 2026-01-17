69a11050906b41245570a8f7b3161d0dad0e5e87
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2026-01-17 16:45:34 +0530

    DSA

--- DSA in Python/Linear_search.py
+++ DSA in Python/Linear_search.py
@@ -1,24 +1,10 @@
 # Linear Search
-# n=[1,2,3,4,5]
-# tar=3
-# for i in range(len(n)):
-#     if n[i]==tar:
-#         print("Target Found at index",i)
-#         break
-# else:
-#     print("Target Not found  in list")
-
-# Linear search using function
-def is_lin(n,tar):
-    for i in range(len(n)):
-        if n[i]==tar:
-            return i
-    return -1
-
-n=[1,2,3,4]
+n=[1,2,3,4,5]
 tar=3
-res=is_lin(n,tar)
-if res!= -1:
-    print("Target found at index",res)
+for i in range(len(n)):
+    if n[i]==tar:
+        print("Target Found at index",i)
+        break
 else:
-    print("Target not in List")
+    print("Target Not found  in list")
+
