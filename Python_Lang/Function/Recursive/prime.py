7246f1cd6b76995b6cca167a186e14a503213a00
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2026-01-21 16:13:00 +0530

    Functions

--- Python_Lang/Function/Recursive/prime.py
+++ Python_Lang/Function/Recursive/prime.py
@@ -6,3 +6,9 @@ def is_prime(n, i=2):
     if n % i == 0:
         return False
     return is_prime(n, i + 1)
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print("Prime number")
+else:
+    print("Not a prime number")
