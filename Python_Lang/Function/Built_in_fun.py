8f66b22a28e98cc5a1ba9dcfaf7557af65278a5f
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2026-01-24 19:10:55 +0530

    List

---
+++ Python_Lang/Function/Built_in_fun.py
@@ -0,0 +1,8 @@
+# Built-in Functions in python
+n=[10,20,30,40]
+print(len(n))           #For length of array
+print(sum(n))           #For Sum of array
+print("Min:",min(n),"Max:",max(n))    #For min and max in an array
+# Enumerate function which gives index and value together
+for a,b in enumerate(n):
+    print(a,b) 
