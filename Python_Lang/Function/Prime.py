ef3efa7accad2e8645831c71863a7e38585be81f
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2026-01-27 21:05:30 +0530

    Functions

---
+++ Python_Lang/Function/Prime.py
@@ -0,0 +1,10 @@
+def prime(n):
+    if n<=1:
+        return False
+    for i in range(2,n):
+        print(f"Factor of {n} is {i}" )
+        if n % i == 0:
+            return False
+    return True
+n=24
+print(prime(n))
